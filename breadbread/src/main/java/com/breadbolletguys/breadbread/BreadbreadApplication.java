7394e48e34084ef49e1fc97b1957a1c0119c9a52
문진수 <mjinsu098@gmail.com>
2025-03-12 10:28:16 +0900

    [chore] : backend project init

---
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/BreadbreadApplication.java
@@ -0,0 +1,13 @@
+package com.breadbolletguys.breadbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadbreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadbreadApplication.class, args);
+	}
+
+}
---
+++ breadbread/src/test/java/com/breadbolletguys/breadbread/BreadbreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadbolletguys.breadbread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadbreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
