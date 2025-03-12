72e8de507660bc302ac950f6831f4d35d9cb45e7
KSH0610 <spacer1@naver.com>
2025-03-12 14:46:43 +0900

    [feat] : 주문 도메인 정의

---
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/order/domain/Order.java
@@ -0,0 +1,53 @@
+package com.breadbolletguys.breadbread.order.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import com.breadbolletguys.breadbread.common.domain.BaseTimeEntity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "bakery_id", nullable = false)
+    private Long bakeryId;
+
+    @Column(name = "seller_id", nullable = false)
+    private Long sellerId;
+
+    @Column(name = "space_id", nullable = false)
+    private Long spaceId;
+
+    @Column(name = "buyer_id")
+    private Long buyerId;
+
+    @Column(name = "name", length = 128, nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDateTime expirationDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "product_state", nullable = false)
+    private ProductState productState;
+}
