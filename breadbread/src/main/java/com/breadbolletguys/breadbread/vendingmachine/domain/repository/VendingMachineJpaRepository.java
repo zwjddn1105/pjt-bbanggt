37e9ee4091e4418df1b3796eda66c82a8e0016df
문진수 <mjinsu098@gmail.com>
2025-03-24 13:08:14 +0900

    Revert "Merge branch 'feat/#1' into 'master'"
    This reverts merge request !4

--- breadbread/src/main/java/com/breadbolletguys/breadbread/vendingmachine/domain/repository/VendingMachineJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.breadbolletguys.breadbread.vendingmachine.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.breadbolletguys.breadbread.vendingmachine.domain.VendingMachine;
-
-public interface VendingMachineJpaRepository extends JpaRepository<VendingMachine, Long> {
-}
