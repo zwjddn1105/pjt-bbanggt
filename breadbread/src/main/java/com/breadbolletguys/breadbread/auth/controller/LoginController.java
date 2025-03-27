465220bc57c6d7401c575bbcef906c86541af1a6
문진수 <mjinsu098@gmail.com>
2025-03-27 14:47:29 +0900

    [fix] : login uri 수정

--- breadbread/src/main/java/com/breadbolletguys/breadbread/auth/controller/LoginController.java
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/auth/controller/LoginController.java
@@ -22,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/v1/auth")
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
