1f68472d568f795be9e0c2d230a40075fdb491b8
문진수 <mjinsu098@gmail.com>
2025-03-27 12:52:51 +0900

    [chore] : Admin, Auth 유저 명세에서 제거 #18

--- breadbread/src/main/java/com/breadbolletguys/breadbread/auth/annotation/AuthUser.java
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/auth/annotation/AuthUser.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
+@Parameter(hidden = true)
 public @interface AuthUser {
 
 }
