9668843493429c7c571c0eca1b1a850b7a5e6237
jinsu868 <mjinsu098@gmail.com>
2025-04-02 22:37:54 +0900

    [feat] : Space Error 코드 추가 #30

--- breadbread/src/main/java/com/breadbolletguys/breadbread/common/exception/ErrorCode.java
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/common/exception/ErrorCode.java
@@ -22,6 +22,7 @@ public enum ErrorCode {
     DUPLICATE_BOOKMARK(10012, "이미 북마크한 빵집입니다."),
     BOOKMARK_NOT_FOUND(10013, "북마크가 존재하지 않습니다."),
     ALREADY_SELLER(10014, "이미 판매자입니다."),
+    NOT_SELLER(10015, "판매자가 아닙니다."),
 
     UNABLE_TO_GET_USER_INFO(2001, "소셜 로그인 공급자로부터 유저 정보를 받아올 수 없습니다."),
     UNABLE_TO_GET_ACCESS_TOKEN(2002, "소셜 로그인 공급자로부터 인증 토큰을 받아올 수 없습니다."),
@@ -47,6 +48,8 @@ public enum ErrorCode {
 
     CANT_REMOVE_OCCUPIED_VENDING_MACHINE(8000, "사용중인 자판기는 삭제할 수 없습니다."),
     NOT_FOUND_VENDING_MACHINE(8001, "해당 자판기를 찾을 수 없습니다."),
+    NOT_FOUND_SPACE(8002, "해당 칸을 찾을 수 없습니다."),
+    ALREADY_PURCHASED_SPACE(8003, "이미 구매된 칸입니다."),
 
     FILE_UPLOAD_FAILED(9000, "파일 업로드에 실패했습니다. 다시 시도해 주세요."),
 
