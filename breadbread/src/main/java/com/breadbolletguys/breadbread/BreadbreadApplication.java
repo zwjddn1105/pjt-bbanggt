422c2b4f46539c1a62e44147fe020be4485f0f4f
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-04-04 12:45:53 +0900

    [chore] : 안 쓰는 API 정리

--- breadbread/src/main/java/com/breadbolletguys/breadbread/BreadbreadApplication.java
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/BreadbreadApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class BreadbreadApplication {
+public class  BreadbreadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BreadbreadApplication.class, args);
