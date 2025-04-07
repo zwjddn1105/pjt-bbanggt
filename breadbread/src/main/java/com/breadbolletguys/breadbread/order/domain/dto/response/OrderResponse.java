aac105e05bb8e371497b2cafa485d5054bb58abe
KSH0610 <spacer1@naver.com>
2025-04-07 15:06:11 +0900

    [chore] : /vendor/{vendingMachineId}/{orderId} 응답 쿼리문 수정 #63

--- breadbread/src/main/java/com/breadbolletguys/breadbread/order/domain/dto/response/OrderResponse.java
+++ breadbread/src/main/java/com/breadbolletguys/breadbread/order/domain/dto/response/OrderResponse.java
@@ -8,8 +8,10 @@ import com.breadbolletguys.breadbread.order.domain.ProductState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderResponse {
@@ -29,4 +31,26 @@ public class OrderResponse {
     private String vendingMachineName;
     private int slotNumber;
     private LocalDateTime paymentDate;
+
+    public OrderResponse(Long orderId, String address, String bakeryName, int price, int salePrice,
+                         int count, String image, ProductState productState, BreadType breadType,
+                         Long bakeryId, Long vendingMachineId, Double latitude, Double longitude,
+                         String vendingMachineName, int slotNumber) {
+        this.orderId = orderId;
+        this.address = address;
+        this.bakeryName = bakeryName;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.count = count;
+        this.image = image;
+        this.productState = productState;
+        this.breadType = breadType;
+        this.bakeryId = bakeryId;
+        this.vendingMachineId = vendingMachineId;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.vendingMachineName = vendingMachineName;
+        this.slotNumber = slotNumber;
+    }
+
 }
